e36eb8994558070c9c5f0b49295110d93921014c
Renish Ponkiya <ponkiyarenish@gmail.com>
2024-03-23 16:09:43 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from 'uuid';
+import { RiEdit2Line } from "react-icons/ri";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 
 function App() {
@@ -11,13 +13,13 @@ function App() {
 
   useEffect(() => {
     let todoString = localStorage.getItem("todos")
-    if(todoString){
-      let todos = JSON.parse(localStorage.getItem("todos")) 
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"))
       setTodos(todos)
     }
-    if(!showFinished){
+    if (!showFinished) {
       setshowFinished(true)
-    }else{
+    } else {
       setshowFinished(false)
     }
   }, [])
@@ -25,56 +27,56 @@ function App() {
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-  
+
 
   const saveToLS = (params) => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }
 
   const toggleFinished = (e) => {
-    if(!showFinished){
+    if (!showFinished) {
       setshowFinished(true)
-    }else{
+    } else {
       setshowFinished(false)
     }
   }
-  
-  
 
 
-  const handleEdit = (e, id)=>{ 
-    let t = todos.filter(i=>i.id === id) 
+
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter(i => i.id === id)
     setTodo(t[0].todo)
-    let newTodos = todos.filter(item=>{
-      return item.id!==id
-    }); 
-    setTodos(newTodos) 
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
     saveToLS()
   }
 
-  const handleDelete= (e, id)=>{  
-    let newTodos = todos.filter(item=>{
-      return item.id!==id
-    }); 
-    setTodos(newTodos) 
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
     saveToLS()
   }
 
-  const handleAdd= ()=>{
-    setTodos([...todos, {id: uuidv4(), todo, isCompleted: false}])
-    setTodo("") 
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
+    setTodo("")
     saveToLS()
   }
-  
-  const handleChange= (e)=>{ 
+
+  const handleChange = (e) => {
     setTodo(e.target.value)
   }
 
-  const handleCheckbox = (e) => { 
-    let id = e.target.name;  
-    let index = todos.findIndex(item=>{
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex(item => {
       return item.id === id;
-    }) 
+    })
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos)
@@ -90,33 +92,40 @@ function App() {
       <div className="container mx-auto pb-5">
         {/* box */}
         <div className="box break-word bg-purple-300 p-5 rounded-xl min-h-[80dvh]">
+          <div className="text-3xl font-bold text-center mb-6">Task Manager</div>
           {/* addTodo */}
-          <div className="addToDo my-5">
-            <div className=" text-lg font-bold ">Add ToDo</div>
-            <input onChange={handleChange} value={todo} className="w-2/5" type="text" placeholder="Enter your ToDo" />
-            <button onClick={handleAdd} disabled={todo.length <= 3} className="bg-purple-800 hover:bg-purple-950 text-white p-2 px-5 rounded-lg mx-5 text-sm disabled:bg-purple-600">Add</button>
+          <div className=" text-2xl font-bold md:ml-3">Add ToDo</div>
+          <div className="addToDo   justify-center items-center">
+            <div className="flex justify-center">
+              <input onChange={handleChange} value={todo} className="w-full h-10 block text-center m-3" type="text" placeholder="Enter your ToDo" />
+            </div>
+            <div className="flex justify-center ">
+              <button onClick={handleAdd} disabled={todo.length <= 3} className="bg-purple-800 hover:bg-purple-950 text-white p-2 px-10 rounded-lg mx-2 text-sm disabled:bg-purple-600 w-full h-10 ">Add</button>
+            </div>
           </div>
 
-          <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
-         <label className='mx-2' htmlFor="show">Show Finished</label>
+          <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} />
+          <label className='mx-2' htmlFor="show">Show Finished</label>
 
-          <div className="text-lg font-bold">Your ToDo</div>
-          {todos.length === 0 ? <div className="flex justify-center mt-5 text-3xl font-bold">Todo list is empty</div> : null}
+          <div className="text-2xl font-bold md:ml-3 mt-3">Your ToDo</div>
+          {todos.length === 0 ? <div className="flex justify-center mt-5 md:text-3xl text-2xl font-bold">Todo list is empty</div> : null}
           <div className="todos">
             {todos.map(item => {
 
               return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex justify-between items-center my-3">
 
-                <div className={` ${item.isCompleted ? "line-through" : ""} text-xl font-medium flex items-center gap-16`}>
+                <div className={` ${item.isCompleted ? "line-through" : ""} md:text-xl font-medium flex items-center md:gap-16 gap-4 text-lg`}>
                   <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} className="w-6 h-6" />
-                  <div className="max-w-[65vw] flex-wrap break-word">{item.todo}</div>
+                  <div className="max-w-[65dvw] flex-wrap break-word">{item.todo}</div>
                 </div>
                 <div className="button flex gap-2 ">
-                  <button onClick={(e) => handleEdit(e, item.id)} className="bg-purple-800 hover:bg-purple-950 text-white py-2 px-3 rounded-lg text-sm">Edit</button>
-                  <button onClick={(e) => handleDelete(e, item.id)} className="bg-purple-800 hover:bg-purple-950 text-white py-2 px-3 rounded-lg mx-2 text-sm">Delete</button>
+                  <button onClick={(e) => handleEdit(e, item.id)} className="bg-purple-800 hover:bg-purple-950 text-white py-2 px-3 rounded-lg text-sm"><img src="assets/edit.svg" alt="" /><RiEdit2Line />
+                  </button>
+                  <button onClick={(e) => handleDelete(e, item.id)} className="bg-purple-800 hover:bg-purple-950 text-white py-2 px-3 rounded-lg md:mx-2 text-sm"><MdOutlineDeleteOutline />
+                  </button>
                 </div>
               </div>
-              
+
             })}
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
     <nav className=" bg-black text-white mb-5">
       <ul className="flex justify-between  gap-11 p-5 items-center">
         <div className="logo">
-          <span className="text-3xl font-bold text-purple-300">itsTodo</span>
+          <span className="md:text-3xl text-2xl font-bold text-purple-300">itsTodo</span>
         </div>
 
-        <div className="flex gap-11">
+        <div className="flex md:gap-11 gap-4">
           <span>
-            <a href="" className="cursor-pointer text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">Home</a>
+            <a href="" className="cursor-pointer md:text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">Home</a>
           </span>
           <span>
-            <a href="" className="cursor-pointer text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">About</a>
+            <a href="" className="cursor-pointer md:text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">About</a>
           </span>
           <span>
-            <a href="" className="cursor-pointer text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">Contact</a>
+            <a href="" className="cursor-pointer md:text-xl hover:text-purple-300 hover:font-semibold transition-all duration-300">Contact</a>
           </span>
         </div>
       </ul>
